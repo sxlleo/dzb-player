95497228b3a59a666a85a603d0a83ba215deeb34
songxiaolin <songxiaolin@aixuexi.com>
2023-03-29 15:10:32 +0800

    upd: 更新rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -2,39 +2,51 @@
  * @Author: songxiaolin songxiaolin@aixuexi.com
  * @Date: 2023-03-23 11:57:00
  * @LastEditors: songxiaolin songxiaolin@aixuexi.com
- * @LastEditTime: 2023-03-23 16:04:01
+ * @LastEditTime: 2023-03-29 15:08:31
  * @FilePath: /penCorrectPlayer/rollup.config.js
  * @Description: 
  */
+const path = require('path')
 const babel = require('@rollup/plugin-babel');
 const json = require('@rollup/plugin-json');
-const resolve = require('@rollup/plugin-node-resolve');
+const nodeResolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 
 const pkg = require('./package.json');
+
+// resolve公共方法
+const resolve = p => path.resolve(__dirname, p)
+
 const plugins = [
-	resolve(), // so Rollup can find `ms`
-	commonjs(), // so Rollup can convert `ms` to an ES module
+	nodeResolve(),
+	commonjs(),
 	json(),
 	babel({
 		presets: ['@babel/preset-env'],
 		exclude: 'node_modules/**',
-		extensions: ['.js', '.vue']
+		extensions: ['.js']
 	}),
 ]
+const outputConfigs = {
+  'esm-bundler': {
+    file: resolve(`dist/${pkg.filename}.esm-bundler.js`),
+    format: `es`
+  },
+  cjs: {
+    file: resolve(`dist/${pkg.filename}.cjs.js`),
+    format: `cjs`
+  },
+}
+
+// 不需要参与bundle的第三方包
+function resolveExternal(){
+	return [
+		...Object.keys(pkg.dependencies || {}),
+		...Object.keys(pkg.devDependencies || {})
+	]
+}
 
 module.exports = [
-// browser-friendly UMD build
-	{
-		input: 'src/index.js',
-		output: {
-			name: 'pencorrectplayer',
-			file: pkg.browser,
-			format: 'umd',
-			banner: '/*eslint-disable */',
-		},
-		plugins
-	},
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
@@ -43,10 +55,8 @@ module.exports = [
 	// `file` and `format` for each target)
 	{
 		input: 'src/index.js',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
+		external: resolveExternal(),
+		plugins,
+		output: Object.values(outputConfigs),
 	}
 ]
