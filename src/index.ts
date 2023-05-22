5fe500f3638f68dcbbf27966eec7c6c8653e7a58
songxiaolin <songxiaolin@aixuexi.com>
2023-05-22 18:56:55 +0800

    modify export name

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@
  * @Author: songxiaolin songxiaolin@aixuexi.com
  * @Date: 2023-05-22 18:02:51
  * @LastEditors: songxiaolin songxiaolin@aixuexi.com
- * @LastEditTime: 2023-05-22 18:23:46
+ * @LastEditTime: 2023-05-22 18:56:18
  * @FilePath: /penCorrectPlayer/src/index.ts
  * @Description: 
  */
 
 export { Events } from './EventsType';
-export { default as CorrectStringPlayer } from './CorrectStringPlayer';
+export { default as PenCorrectPlayer } from './CorrectStringPlayer';
 export type { PageCanvas } from './MultiPages'
